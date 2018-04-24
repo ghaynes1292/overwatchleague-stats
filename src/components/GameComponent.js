aa12a671ba0ed6965907e6be4c5d17e3c3e222b0
Gavin Haynes <gavin.haynes@moove-it.com>
2018-04-24 11:04:35 -0500

    Show winner overlay for each game in team popup

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -16,6 +16,7 @@ const styles = {
     gridTemplateRows: '40px 1fr 1fr',
     gridTemplateAreas: "'details details' 'team thumbnail' 'opponent thumbnail'",
     margin: '20px 10px',
+    position: 'relative',
   },
   gameThumbnail: {
     gridArea: 'thumbnail',
@@ -61,24 +62,40 @@ const styles = {
     width: '100%',
     display: 'flex',
     justifyContent: 'space-around'
+  },
+  winnerOverlay: {
+    position: 'absolute',
+    textAlign: 'center',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  overlayImage: {
+    height: '75%',
+    marginTop: '8%',
   }
 };
 
-function generateShadow(hex) {
+function generateOverlay(hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   const uicolors = [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
   const newColors = uicolors.map((color, _, colors) => color === Math.max.apply(null, colors) ? Math.round(color * 1.5) : color)
   const colorRgb = `${newColors[0]}, ${newColors[1]}, ${newColors[2]}`
-  return `0px 6px 13px -6px rgba(${colorRgb}, 0.5),
-  0px 10px 10px 3px rgba(${colorRgb}, 0.3),
-  0px 8px 12px 7px rgba(${colorRgb}, 0.12)`;
+  return `rgba(${colorRgb}, 0.67)`;
 }
 
 function GameComponent(props) {
   const { classes, game, team, opponent, teamStats, opponentStats } = props;
-  const winner = get(find(game.team, ['id', game.id]), 'winner', null)
+  const winner = get(find(game.team, ['id', game.id]), 'winner', null);
+  console.log('winner', team, winner)
   return (
-    <Paper className={classes.root} elevation={20} style={{ boxShadow: generateShadow(winner !== null ? winner ? team.colors.primary.color : opponent.colors.primary.color : '#000000')}}>
+    <Paper className={classes.root} elevation={4}>
+      {winner !== null && (
+        <div className={classes.winnerOverlay} style={{ backgroundColor: generateOverlay(winner ? team.colors.primary.color : opponent.colors.primary.color) }}>
+          <img className={classes.overlayImage} src={winner ? (team.logo.alt || team.logo.main).png : (opponent.logo.alt || opponent.logo.main).png} />
+        </div>
+      )}
       <div className={classes.gameThumbnail} style={{ backgroundImage: `url(${game.icon})`.replace(/(["])/, '') }}/>
       <div className={classes.gameDetails}>
         <Typography variant="subheading" gutterBottom>
